269deec7ee52c1f23e9109851e15be8d157c4e97
Cloud User <centos@ip-172-31-20-77.eu-west-1.compute.internal>
2020-04-26 01:52:10 +0000

    push

--- task1/route_53.tf
+++ task1/route_53.tf
@@ -1,9 +1,5 @@
-# resource "aws_route53_zone" "main" {
-#   name = "kenvpt.com"
-#   delegation_set_id = "${data.aws_route53_delegation_set.dset}"
-# }
 resource "aws_route53_record" "kenvpt" {
-  zone_id = "Z10047533N9ED5L3STY30"
+  zone_id = "${var.delegation_set}"
   name    = "${var.domain_name}"
   type    = "A"
   ttl     = "300"
