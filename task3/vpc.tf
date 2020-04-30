ee9700e8e3e26cafc74875aaaed11a97e3c27a15
Cloud User <centos@ip-172-31-20-77.eu-west-1.compute.internal>
2020-04-30 22:49:48 +0000

    push

--- task3/vpc.tf
+++ task3/vpc.tf
@@ -3,6 +3,6 @@ resource "aws_vpc" "my_vpc" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
     enable_dns_support = true
-    tags = "${var.tags}"
+    tags = "${merge(var.tags, map("Name", "my_vpc"))}"
 
 }
