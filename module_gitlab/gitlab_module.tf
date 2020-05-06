47db6d6f822afc5b815eefd03d1a092c6661c383
Cloud User <centos@ip-172-31-20-77.eu-west-1.compute.internal>
2020-05-06 01:40:05 +0000

    push

--- module_gitlab/gitlab_module.tf
+++ module_gitlab/gitlab_module.tf
@@ -1,22 +1,22 @@
 module "gitlab1" {
     source = "/home/centos/terraform-iaac-ken/module_gitlab/gitlab_project"
     region = "us-east-1"
-    vpc_cidr = "10.0.0.0/16"
+    vpc_cidr = "12.0.0.0/16"
     public_subnet_cidr = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"
+        "12.0.1.0/24",
+        "12.0.2.0/24",
+        "12.0.3.0/24"
 ]
     private_subnet_cidr = [
-       "10.0.11.0/24",
-      "10.0.12.0/24",
-      "10.0.13.0/24"
+         "12.0.11.0/24",
+         "12.0.12.0/24",
+         "12.0.13.0/24"
     ]
 
     instance_type = "t2.xlarge"
     key_name = "my_key"
     public_key_path = "~/.ssh/id_rsa.pub"
-    domain_name = "www.gitlab.kenvpt.com"
+    domain_name = "www.prod.gitlab.com"
     delegation_set = "Z10047533N9ED5L3STY30"
     tags = {
      Environment = "Gitlab"
@@ -30,22 +30,22 @@ module "gitlab1" {
 module "gitlab2" {
     source = "/home/centos/terraform-iaac-ken/module_gitlab/gitlab_project"
     region = "us-east-2"
-    vpc_cidr = "10.0.0.0/16"
+    vpc_cidr = "11.0.0.0/16"
     public_subnet_cidr = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"
+        "11.0.1.0/24",
+        "11.0.2.0/24",
+        "11.0.3.0/24"
 ]
     private_subnet_cidr = [
-       "10.0.11.0/24",
-      "10.0.12.0/24",
-      "10.0.13.0/24"
+        "11.0.11.0/24",
+        "11.0.12.0/24",
+        "11.0.13.0/24"
     ]
 
     instance_type = "t2.xlarge"
     key_name = "my_key"
     public_key_path = "~/.ssh/id_rsa.pub"
-    domain_name = "www.gitlab.kenvpt.com"
+    domain_name = "www.dev.gitlab.com"
     delegation_set = "Z10047533N9ED5L3STY30"
     tags = {
      Environment = "Gitlab"
