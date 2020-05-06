27da8b181398b8cfed998b0a68ea354d2aa32108
Cloud User <centos@ip-172-31-20-77.eu-west-1.compute.internal>
2020-05-06 02:21:25 +0000

    push

--- module_gitlab/gitlab_module.tf
+++ module_gitlab/gitlab_module.tf
@@ -1,16 +1,16 @@
 module "gitlab1" {
     source = "/home/centos/terraform-iaac-ken/module_gitlab/gitlab_project"
-    region = "us-east-1"
-    vpc_cidr = "12.0.0.0/16"
+    region = "us-west-1"
+    vpc_cidr = "192.168.0.0/16"
     public_subnet_cidr = [
-        "12.0.1.0/24",
-        "12.0.2.0/24",
-        "12.0.3.0/24"
+        "192.168.1.0/24",
+        "192.168.2.0/24",
+        "192.168.3.0/24"
 ]
     private_subnet_cidr = [
-         "12.0.11.0/24",
-         "12.0.12.0/24",
-         "12.0.13.0/24"
+         "192.168.11.0/24",
+         "192.168.12.0/24",
+         "192.168.13.0/24"
     ]
 
     instance_type = "t2.xlarge"
