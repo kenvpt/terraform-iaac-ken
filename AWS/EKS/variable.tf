1a6eadfa16c72b11161d227f6d37fc7e604bd37e
Cloud User <centos@ip-172-31-20-77.eu-west-1.compute.internal>
2020-05-12 01:51:49 +0000

    push

---
+++ AWS/EKS/variable.tf
@@ -0,0 +1,17 @@
+variable "vpc_id" {}
+variable "subnet1" {}
+variable "subnet2" {}
+variable "subnet3" {}
+variable "cluster_name" {}
+variable "environment" {}
+variable "instance_type" {}
+variable "asg_max_size" {}
+variable "asg_min_size" {}
+variable "cluster_version" {}
+variable "s3_folder_project" {}
+variable "s3_folder_region" {}
+variable "s3_folder_type" {}
+variable "s3_tfstate_file" {}
+variable "s3_bucket" {}
+variable "region" {}
+variable "asg_desired_capacity" {}
